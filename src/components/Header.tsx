eaf149d13f974aa5d76b7354068472b5a557b12e
acopier <acopierlive@gmail.com>
2024-03-29 14:14:17 +0100

    header restructure, add image optimalisation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,63 +5,78 @@ import useDarkMode from '@/hooks/useDarkMode';
 
 function Header() {
   const darkMode = useDarkMode();
+  const handleClick = () => {
+    (document.activeElement as HTMLElement)?.blur();
+  };
   return (
-    <nav className='navbar shadow-md sticky font-skaut'>
-      <div className='flex-1 navbar-start'>
-        <Link href='/' className='hover:opacity-75'>
-          <Image
-            alt='skaut'
-            width={256}
-            height={256}
-            src={`/skaut-${darkMode === true ? 'light' : 'dark'}.png`}
-          />
-        </Link>
-        <span className='font-bold'>Web skautského oddílu Jednička HK</span>
-      </div>
-      <div className='flex-none navbar-end'>
-        <ul className='menu menu-horizontal px-1'>
-          <li>
-            <Link href='/odborky'>Odborky a vlčci</Link>
-          </li>
-          <li>
-            <details>
-              <summary>Oddíl</summary>
-              <ul className='bg-base-100 rounded-t-none'>
-                <li>
+    <nav className='navbar shadow-md sticky font-skaut rounded-box'>
+      <div className='navbar bg-base-300 rounded-box'>
+        <div className='flex-1 px-2 lg:flex-none'>
+          <Link href='/' className='hover:opacity-75'>
+            <Image
+              alt='skaut'
+              width={128}
+              height={128}
+              src={`/skaut-${darkMode === true ? 'light' : 'dark'}.png`}
+            />
+          </Link>
+          Web skautského oddílu Jednička HK
+        </div>
+        <div className='flex justify-end flex-1 px-2'>
+          <div className='flex items-center text-sm'>
+            <div className='dropdown dropdown-end'>
+              <div
+                tabIndex={0}
+                role='button'
+                className='btn btn-ghost rounded-btn'
+              >
+                Oddíl
+              </div>
+              <ul
+                tabIndex={0}
+                className='menu dropdown-content z-[1] p-2 shadow bg-base-100 rounded-box w-52 mt-4'
+              >
+                <li onClick={handleClick}>
                   <Link href='/druziny'>Družiny</Link>
                 </li>
-                <li>
+                <li onClick={handleClick}>
                   <Link href='/akce'>Plánované akce</Link>
                 </li>
-                <li>
+                <li onClick={handleClick}>
                   <Link href='/casopis'>Jednička hlásí</Link>
                 </li>
-                <li>
+                <li onClick={handleClick}>
                   <Link href='/sponzori'>Sponzoři</Link>
                 </li>
-                <li>
+                <li onClick={handleClick}>
                   <Link href='/s-sebou'>Co s sebou</Link>
                 </li>
-                <li>
+                <li onClick={handleClick}>
                   <Link href='/dokumenty'>Dokumenty</Link>
                 </li>
               </ul>
-            </details>
-          </li>
-          <li>
-            <Link href='/ucet'>Účet</Link>
-          </li>
-          <li>
+            </div>
+            <div
+              tabIndex={0}
+              role='button'
+              className='btn btn-ghost rounded-btn'
+            >
+              <Link href='/odborky'>Odborky a vlčci</Link>
+            </div>
+            <div
+              tabIndex={0}
+              role='button'
+              className='btn btn-ghost rounded-btn'
+            >
+              <Link href='/kontakt'>Kontakt</Link>
+            </div>
             <label className='swap swap-rotate'>
               <input
                 type='checkbox'
                 className='theme-controller'
                 value={darkMode === true ? 'light' : 'dark'}
-                aria-label='darkMode'
-                id='darkMode'
               />
 
-              {/* sun icon */}
               <svg
                 className='swap-off fill-current w-10 h-10'
                 xmlns='http://www.w3.org/2000/svg'
@@ -70,7 +85,6 @@ function Header() {
                 <path d='M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z' />
               </svg>
 
-              {/* moon icon */}
               <svg
                 className='swap-on fill-current w-10 h-10'
                 xmlns='http://www.w3.org/2000/svg'
@@ -79,8 +93,8 @@ function Header() {
                 <path d='M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z' />
               </svg>
             </label>
-          </li>
-        </ul>
+          </div>
+        </div>
       </div>
     </nav>
   );

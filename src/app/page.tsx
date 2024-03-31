423c738d6e0fc6afe36e975978c6d82953a10168
acopier <acopierlive@gmail.com>
2024-03-31 09:20:27 +0200

    moved the paragraph from header to content, fixed scaling on mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,31 @@ function Page() {
   return (
     <>
       <Hero />
-      <div className='card card-compact w-96 h-[31rem] bg-base-100 float-left overflow-hidden mx-1'>
+      <div className='float-left overflow-hidden mx-1 m-1'>
+        <div className='card card-compact bg-base-100 mx-1'>
+          <div className='card-body'>
+            <p className='font-themix rounded-box font-bold text-lg bg-base-200 lg:w-[29rem] w-[22rem]'>
+              Jsme skautský oddíl z Hradce Králové - Malšovic. V našem oddíle se
+              schází kluci i holky z celého Hradce Králové a okolí. Naší činnost
+              tvoří družinové schůzky, které se konají pravidelně každé úterý v
+              naší klubovně. Také pořádáme řadu dalších akcí. Několikrát do roka
+              jezdíme na víkendové akce do širokého okolí. O jarních prázdninách
+              pořádáme zimní tábor a v létě jezdíme na čtrnáctidenní letní tábor
+              pod stany.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className='card card-compact bg-base-100 float-left overflow-hidden mx-1 m-1'>
         <figure>
           <iframe
             src='https://frame.mapy.cz/s/fokuboruzo'
-            width='700'
             height='300'
             allowFullScreen={false}
             loading='lazy'
             referrerPolicy='no-referrer-when-downgrade'
             title='map'
+            className='lg:w-[40rem] w-96 h-[19.5rem]'
           />
         </figure>
         <div className='card-body'>
@@ -29,8 +44,8 @@ function Page() {
           </p>
         </div>
       </div>
-      <div className='float-right overflow-hidden mx-1'>
-        <div className='card card-compact w-96 bg-base-100'>
+      <div className='float-right overflow-hidden mx-1 m-1 items-center'>
+        <div className='card card-compact lg:w-56 w-96 h-[32.7rem] bg-base-100'>
           <figure>
             <Image
               src='/siggi.jpg'
@@ -49,8 +64,8 @@ function Page() {
           </div>
         </div>
       </div>
-      <div className='float-right overflow-hidden mx-1'>
-        <div className='card card-compact w-96 bg-base-100'>
+      <div className='float-right overflow-hidden mx-1 m-1'>
+        <div className='card card-compact lg:w-56 w-96 bg-base-100'>
           <figure>
             <Image
               src='/svaca.jpg'
@@ -71,8 +86,8 @@ function Page() {
           </div>
         </div>
       </div>
-      <div className='float-right overflow-hidden mx-1'>
-        <div className='card card-compact w-96 bg-base-100'>
+      <div className='float-right overflow-hidden mx-1 m-1'>
+        <div className='card card-compact lg:w-56 w-96 bg-base-100'>
           <figure>
             <Image
               src='/pepek.jpg'
@@ -83,7 +98,7 @@ function Page() {
             />
           </figure>
           <div className='card-body'>
-            <h2 className='card-title justify-center'>Pepek</h2>
+            <h2 className='card-title justify-center lg:px-0 px-5'>Pepek</h2>
             <div className='divider m-0' />
             <p className='italic text-lg'>zástupce vůdce oddílu pro vlčata</p>
             <p className='text-lg'>+420 603 368 588</p>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -31,15 +31,6 @@ function Hero() {
           <h1 className='mb-5 text-4xl font-bold font-skaut bg-gray-500 rounded-box p-2 text-center'>
             1.Oddíl Junáka Hradec Králové
           </h1>
-          <p className='font-themix rounded-box px-3 p-3 font-bold text-lg bg-base-200'>
-            Jsme skautský oddíl z Hradce Králové - Malšovic. V našem oddíle se
-            schází kluci i holky z celého Hradce Králové a okolí. Naší činnost
-            tvoří družinové schůzky, které se konají pravidelně každé úterý v
-            naší klubovně. Také pořádáme řadu dalších akcí. Několikrát do roka
-            jezdíme na víkendové akce do širokého okolí. O jarních prázdninách
-            pořádáme zimní tábor a v létě jezdíme na čtrnáctidenní letní tábor
-            pod stany.
-          </p>
         </div>
       </div>
     </div>

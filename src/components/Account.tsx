e5f1bde012d41edc64bfa0683c3fcf28431ff4ca
acopier <acopierlive@gmail.com>
2024-04-07 07:58:47 +0200

    added checks for fields

--- src/components/Account.tsx
+++ src/components/Account.tsx
@@ -4,15 +4,17 @@ import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 
 function Account() {
-  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
   const router = useRouter();
-
+  const disabled =
+    email === '' || username === '' || password === '' ? true : false;
   async function loginUser() {
     try {
       const user = await pb
         .collection('users')
-        .authWithPassword(username, password);
+        .authWithPassword(email, password);
       currentUser.set(user);
       typeof window !== 'undefined' ? location.reload() : null;
     } catch (error) {
@@ -24,6 +26,7 @@ function Account() {
     try {
       const data = {
         username,
+        email,
         password,
         passwordConfirm: password,
       };
@@ -45,14 +48,29 @@ function Account() {
   return (
     <>
       {currentUser.get() === null ? (
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-          }}
-        >
+        <form onSubmit={(e) => e.preventDefault()}>
           <div className='flex justify-center'>
             <div>
-              <label className='input input-bordered flex items-center gap-2'>
+              <label className='input input-bordered flex items-center gap-2 mt-1'>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  viewBox='0 0 16 16'
+                  fill='currentColor'
+                  className='w-4 h-4 opacity-70'
+                >
+                  <path d='M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z' />
+                  <path d='M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z' />
+                </svg>
+                <input
+                  type='email'
+                  className='grow'
+                  placeholder='E-mail'
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </label>
+              <label className='input input-bordered flex items-center gap-2 mt-1'>
                 <svg
                   xmlns='http://www.w3.org/2000/svg'
                   viewBox='0 0 16 16'
@@ -64,9 +82,10 @@ function Account() {
                 <input
                   type='text'
                   className='grow'
-                  placeholder='Username'
+                  placeholder='Uživatelské jméno'
                   value={username}
                   onChange={(e) => setUsername(e.target.value)}
+                  required
                 />
               </label>
               <label className='input input-bordered flex items-center gap-2 mt-1'>
@@ -86,22 +105,43 @@ function Account() {
                   type='password'
                   className='grow'
                   value={password}
+                  placeholder='Heslo'
                   onChange={(e) => setPassword(e.target.value)}
+                  required
                 />
               </label>
-              <button onClick={loginUser} className='btn btn-block mt-1'>
-                Log in
-              </button>
-              <button onClick={createUser} className='btn btn-block mt-1'>
-                Sign Up
-              </button>
+              <input
+                type='submit'
+                onClick={loginUser}
+                className='btn btn-block mt-1'
+                value='Přihlásit se'
+                disabled={disabled}
+              />
+              <input
+                type='submit'
+                onClick={createUser}
+                className='btn btn-block mt-1'
+                value='Zaregistrovat se'
+                disabled={disabled}
+              />
             </div>
           </div>
         </form>
       ) : (
         <>
-          <p>Logged in as: {currentUser.get()?.username}</p>
-          <button onClick={logoutUser}>Log out</button>
+          <div className='flex justify-end'>
+            <div className='placeholder rounded-box'>
+              <div className='bg-neutral text-neutral-content rounded-box p-3 h-16'>
+                <span className='text-3xl'>{currentUser.get()?.username}</span>
+              </div>
+            </div>
+            <button
+              onClick={logoutUser}
+              className='btn btn-ghost btn-outline h-16'
+            >
+              Odhlásit se
+            </button>
+          </div>
         </>
       )}
     </>

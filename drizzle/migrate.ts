e5cc3e2a9c8efdf374b9506903cae504655b8668
rgb <44110952+acopier@users.noreply.github.com>
2024-04-25 23:00:19 +0200

    Update migrate.ts

--- drizzle/migrate.ts
+++ drizzle/migrate.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import { migrate } from 'drizzle-orm/mysql2/migrator';
-import { db, connection } from '../src/server/db/schema';
+import { db, connection } from '../src/server/db';
 // This will run migrations on the database, skipping the ones already applied
 await migrate(db, { migrationsFolder: './drizzle/migrations' });
 // Don't forget to close the connection, otherwise the script will hang
